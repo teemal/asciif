9d72e75b75834735596ff399760b66349cbc3e13
teemal <taymal1987@gmail.com>
2020-02-26 17:05:54 -0800

    add and update gitignore for gifs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use image::gif::Decoder;
+use image::AnimationDecoder;
+use image::ImageFormat::JPEG;
+
+use std::fs::File;
+
+fn decode() {
+    // Decode a gif into frames
+    let file_in = File::open("devitobb.gif").expect("Could not open file");
+    let mut decoder = Decoder::new(file_in).expect("Could not create decoder");
+
+    let frames = decoder.into_frames();
+    let frames = frames.collect_frames().expect("error decoding gif");
+
+    let mut i = 0;
+    for f in frames {
+        f.into_buffer().save_with_format(
+            format!("dist/media/{}.jpg", i),
+            JPEG
+        ).expect("Could not save frame");
+        i += 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    decode();
 }
